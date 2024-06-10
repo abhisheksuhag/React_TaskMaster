da9c9aa14ee4a5580e06cbd78886ff981e685853
abhisheksuhag <abhisheksuhag1010@gmail.com>
2024-06-10 11:09:49 +0530

    basic layout

---
+++ ToDo-ReactContext/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-gray-800'>
+        <h1 className=' text-white text-center'>TO-DO APP</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
